776265a7461615329510b4feb99a9a26353788e1
queckezz <fabian.eichenberger@gmail.com>
2013-11-16 17:16:43 +0100

    remove logs()

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ function vendor (env) {
   var dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + occurences[1] + ')', 'i'))[1];
   var js = occurences[1][0].toUpperCase() + occurences[1].substr(1)
 
-  console.log(occurences, dom, js);
-
   if (env === 'js') return js;
   if (env === 'dom') return dom;
   if (env === 'css') return occurences[0];
